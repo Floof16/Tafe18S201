d1491602e433dfd4b1acca85f468b27479fa55c7
Allan Morella <allanmorella@gmail.com>
2018-09-04 17:14:22 +0930

    personalInfo.xaml added: "add","delete", "edit" buttons, a listview. modified labels to be more user friendly.
    PersonalInfo.xaml.cs added:
    database connection, created a table, delete method, add method, edit method.
    
    personalInfo.cs Removed "CompanyName" property as it was not supposed to be there

--- InstaRichie/Models/PersonalInfo.cs
+++ InstaRichie/Models/PersonalInfo.cs
@@ -9,16 +9,13 @@ namespace StartFinance.Models
     class PersonalInfo
     {
         [PrimaryKey, AutoIncrement]
-        public int PersonalID { get; set; }
+        public string PersonalID { get; set; }
 
         [NotNull]
         public string FirstName { get; set; }
 
         [NotNull]
         public string LastName { get; set; }
-
-        [NotNull]
-        public string CompanyName { get; set; }
 
         [NotNull]
         public string DOB { get; set; }
--- InstaRichie/Views/PersonalInfo.xaml.cs
+++ InstaRichie/Views/PersonalInfo.xaml.cs
@@ -14,6 +14,9 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using StartFinance.Models;
 using StartFinance.ViewModels;
+using Windows.UI.Popups;
+using SQLite.Net;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace StartFinance.Views
@@ -25,29 +28,151 @@ namespace StartFinance.Views
     {
         private MainPageViewModel viewModel;
 
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+
         public void NewPage()
         {
             this.InitializeComponent();
-            viewModel = new MainPageViewModel();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            /// Initializing a database
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+
+            Results();
         }
         public PersonalInfo()
         {
             this.InitializeComponent();
         }
-
+        public void Results()
+        {
+            // Creating table
+            conn.CreateTable<PersonalInfo>();
+            var query = conn.Table<PersonalInfo>();
+            PersonalInfoList.ItemsSource = query.ToList();
+        }
         private void cancel_click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Views.MainPage));
         }
 
-        private void save_click(object sender, RoutedEventArgs e)
+        private async void save_click(object sender, RoutedEventArgs e)
         {
-            PersonalInfo P = new PersonalInfo();
+            try
+            {
+                // checks if account name is null
+                if (PersonalID.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("ID not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (FirstName.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("First Name not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (LastName.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Last Name not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (DOB.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Date of Birth not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (Gender.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Gender not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (Email.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Email not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                if (Mobile.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("Mobile not Entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
 
-            P.M
 
+                else
+                {   // Inserts the data
+                    conn.Insert(new Models.PersonalInfo()
+                    {
+                        PersonalID = PersonalID.Text.ToString(),
+                        FirstName = FirstName.Text.ToString(),
+                        LastName = LastName.Text.ToString(),
+                        DOB = DOB.Text.ToString(),
+                        Gender = Gender.Text.ToString(),
+                        Email = Email.Text.ToString(),
+                        phoneNumber = Mobile.Text.ToString()
+                    });
+                    Results();
+                }
 
-            P.PersonalID = txtID.Text;
+            }
+            catch (Exception ex)
+            {   // Exception to display when amount is invalid or not numbers
+                 if (ex is SQLiteException)
+                {
+                    MessageDialog dialog = new MessageDialog("ID already exist, Try Different ID", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else
+                {
+                    /// no idea
+                }
+
+            }
+
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
+
+        private async void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog ShowConf = new MessageDialog("Deleting this Profile will delete all attributes inside this profile", "Important");
+            ShowConf.Commands.Add(new UICommand("Yes, Delete")
+            {
+                Id = 0
+            });
+            ShowConf.Commands.Add(new UICommand("Cancel")
+            {
+                Id = 1
+            });
+            ShowConf.DefaultCommandIndex = 0;
+            ShowConf.CancelCommandIndex = 1;
+
+            var result = await ShowConf.ShowAsync();
+            if ((int)result.Id == 0)
+            {
+                // checks if data is null else inserts
+                try
+                {
+                    string ID = ((PersonalInfo)PersonalInfoList.SelectedItem).PersonalID.ToString() ;
+                    var querydel = conn.Query<PersonalInfo>("DELETE FROM PersonalInfo WHERE PersonalID='" + ID + "'");
+                    Results();
+                }
+                catch (NullReferenceException)
+                {
+                    MessageDialog ClearDialog = new MessageDialog("Please select the item to Delete", "Oops..!");
+                    await ClearDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                //
+            }
+        }
+
+        private void EditItem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
