99b01998684195ac98fe8315439c831fe60a91a9
allan <allanmorella@gmail.com>
2018-09-10 13:56:40 +0930

    updated PersonalInfo.cs to have all necessary methods

--- InstaRichie/Views/PersonalInfo.xaml.cs
+++ InstaRichie/Views/PersonalInfo.xaml.cs
@@ -16,6 +16,7 @@ using StartFinance.Models;
 using StartFinance.ViewModels;
 using Windows.UI.Popups;
 using SQLite.Net;
+using SQLite;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,10 +27,11 @@ namespace StartFinance.Views
     /// </summary>
     public sealed partial class PersonalInfo : Page
     {
-       
+
         SQLiteConnection conn; // adding an SQLite connection
         string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
-
+        string Gender;
+        string DOB;
         public void NewPage()
         {
             this.InitializeComponent();
@@ -43,17 +45,55 @@ namespace StartFinance.Views
         {
             this.InitializeComponent();
         }
+
+
+
         public void Results()
         {
-            // Creating table
-            conn.CreateTable<Models.PersonalInfo>();
-            var query = conn.Table<Models.PersonalInfo>();
-            PersonalInfoList.ItemsSource = query.ToList();
+            //Creating table
+            conn = new SQLite.Net.SQLiteConnection(new SQLite.Net.Platform.WinRT.SQLitePlatformWinRT(), path);
+            conn.CreateTable<StartFinance.Models.PersonalInfo>();
+            var query1 = conn.Table<StartFinance.Models.PersonalInfo>();
+            PersonalInfoList.ItemsSource = query1.ToList();
         }
+
+
+
+
         private void cancel_click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(Views.MainPage));
         }
+
+
+
+        private void rdoMale_Checked(object sender, RoutedEventArgs e)
+        {
+            Gender = "Male";
+            MessageDialog dialog = new MessageDialog("The gender you selecected is: " + Gender);
+        }
+        private void rdoFemale_Checked(object sender, RoutedEventArgs e)
+        {
+            Gender = "Female";
+            MessageDialog dialog = new MessageDialog("The gender you selecected is: " + Gender);
+        }
+        private void DateOfBirth(string selectedDate)
+        {
+            DOB = selectedDate;
+        }
+        private void dateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            if (args.NewDate != null)
+            {
+
+                var date = birthDay.Date;
+                DateTime time = date.Value.DateTime;
+                var formatedtime = time.ToString("dd/MM/yyyy");
+                System.Diagnostics.Debug.WriteLine(formatedtime);
+                DateOfBirth(formatedtime);
+            }
+        }
+
 
         private async void save_click(object sender, RoutedEventArgs e)
         {
@@ -75,16 +115,6 @@ namespace StartFinance.Views
                     MessageDialog dialog = new MessageDialog("Last Name not Entered", "Oops..!");
                     await dialog.ShowAsync();
                 }
-                if (DOB.Text.ToString() == "")
-                {
-                    MessageDialog dialog = new MessageDialog("Date of Birth not Entered", "Oops..!");
-                    await dialog.ShowAsync();
-                }
-                if (Gender.Text.ToString() == "")
-                {
-                    MessageDialog dialog = new MessageDialog("Gender not Entered", "Oops..!");
-                    await dialog.ShowAsync();
-                }
                 if (Email.Text.ToString() == "")
                 {
                     MessageDialog dialog = new MessageDialog("Email not Entered", "Oops..!");
@@ -101,11 +131,11 @@ namespace StartFinance.Views
                 {   // Inserts the data
                     conn.Insert(new Models.PersonalInfo()
                     {
-                        PersonalID = PersonalID.Text.ToString(),
+                        PersonalID = int.Parse(PersonalID.Text),
                         FirstName = FirstName.Text.ToString(),
                         LastName = LastName.Text.ToString(),
-                        DOB = DOB.Text.ToString(),
-                        Gender = Gender.Text.ToString(),
+                        DOB = DOB,
+                        Gender = Gender,
                         Email = Email.Text.ToString(),
                         phoneNumber = Mobile.Text.ToString()
                     });
@@ -115,7 +145,7 @@ namespace StartFinance.Views
             }
             catch (Exception ex)
             {   // Exception to display when amount is invalid or not numbers
-                 if (ex is SQLiteException)
+                if (ex is SQLiteException)
                 {
                     MessageDialog dialog = new MessageDialog("ID already exist, Try Different ID", "Oops..!");
                     await dialog.ShowAsync();
@@ -129,10 +159,15 @@ namespace StartFinance.Views
             }
 
         }
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-            Results();
-        }
+
+
+
+
+
+
+
+
+
 
         private async void DeleteItem_Click(object sender, RoutedEventArgs e)
         {
@@ -151,11 +186,11 @@ namespace StartFinance.Views
             var result = await ShowConf.ShowAsync();
             if ((int)result.Id == 0)
             {
-                
+
                 try
                 {
-                    string ID = ((PersonalInfo)PersonalInfoList.SelectedItem).PersonalID.ToString() ;
-                    var querydel = conn.Query<PersonalInfo>("DELETE FROM PersonalInfo WHERE PersonalID='" + ID + "'");
+                    int ID = ((Models.PersonalInfo)PersonalInfoList.SelectedItem).PersonalID;
+                    var querydel = conn.Query<Models.PersonalInfo>("DELETE FROM PersonalInfo WHERE PersonalID='" + ID + "'");
                     Results();
                 }
                 catch (NullReferenceException)
@@ -166,16 +201,21 @@ namespace StartFinance.Views
             }
             else
             {
-               Frame.Navigate(typeof(Views.PersonalInfo));
+                MessageDialog dialog = new MessageDialog("Nothing was Deleted");
             }
         }
+
+
+
+
+
 
         private async void EditItem_Click(object sender, RoutedEventArgs e)
         {
             try
             {
                 // checks if account name is null
-                if (PersonalID.Text.ToString() == "")
+                if (PersonalID.Text == "")
                 {
                     MessageDialog dialog = new MessageDialog("ID not Entered", "Oops..!");
                     await dialog.ShowAsync();
@@ -188,16 +228,6 @@ namespace StartFinance.Views
                 if (LastName.Text.ToString() == "")
                 {
                     MessageDialog dialog = new MessageDialog("Last Name not Entered", "Oops..!");
-                    await dialog.ShowAsync();
-                }
-                if (DOB.Text.ToString() == "")
-                {
-                    MessageDialog dialog = new MessageDialog("Date of Birth not Entered", "Oops..!");
-                    await dialog.ShowAsync();
-                }
-                if (Gender.Text.ToString() == "")
-                {
-                    MessageDialog dialog = new MessageDialog("Gender not Entered", "Oops..!");
                     await dialog.ShowAsync();
                 }
                 if (Email.Text.ToString() == "")
@@ -214,17 +244,40 @@ namespace StartFinance.Views
 
                 else
                 {   // Updates the data
-                    conn.Update(new Models.PersonalInfo()
+                    MessageDialog ShowConf = new MessageDialog("Are you sure you want to make these changes", "Important");
+                    ShowConf.Commands.Add(new UICommand("Yes, Edit")
                     {
-                        PersonalID = PersonalID.Text.ToString(),
-                        FirstName = FirstName.Text.ToString(),
-                        LastName = LastName.Text.ToString(),
-                        DOB = DOB.Text.ToString(),
-                        Gender = Gender.Text.ToString(),
-                        Email = Email.Text.ToString(),
-                        phoneNumber = Mobile.Text.ToString()
+                        Id = 0
                     });
-                    Results();
+                    ShowConf.Commands.Add(new UICommand("Cancel")
+                    {
+                        Id = 1
+                    });
+                    ShowConf.DefaultCommandIndex = 0;
+                    ShowConf.CancelCommandIndex = 1;
+                    var result = await ShowConf.ShowAsync();
+                    if ((int)result.Id == 0)
+                    {
+
+                        try
+                        {
+                            int ID = ((Models.PersonalInfo)PersonalInfoList.SelectedItem).PersonalID;
+                            var queryEdit = conn.Query<Models.PersonalInfo>("UPDATE PersonalInfo SET FirstName='" + FirstName.Text.ToString() + "', LastName = '" + LastName.Text.ToString() +
+                                                              "', DOB ='" + DOB + "', Gender='" + Gender + "', Email ='" + Email.Text.ToString() + "', phoneNumber ='" + Mobile.Text.ToString() +
+                                                                "'WHERE PersonalID ='" + ID + "'");
+                            Results();
+                        }
+                        catch (NullReferenceException)
+                        {
+                            MessageDialog ClearDialog = new MessageDialog("Please select the item to Edit", "Oops..!");
+                            await ClearDialog.ShowAsync();
+                        }
+                    }
+                    else
+                    {
+                        MessageDialog dialog = new MessageDialog("No Edits Made");
+                    }
+
                 }
 
             }
@@ -232,18 +285,23 @@ namespace StartFinance.Views
             {   // Exception to display when ID is invalid 
                 if (ex is SQLiteException)
                 {
-                    MessageDialog dialog = new MessageDialog("ID already exist, Try Different ID", "Oops..!");
+                    MessageDialog dialog = new MessageDialog("Incorrect ID", "Oops..!");
                     await dialog.ShowAsync();
                 }
                 else
                 {
                     Frame rootFrame = Window.Current.Content as Frame;
 
-                 
-                   Frame.Navigate(typeof(Views.PersonalInfo));
+
+                    Frame.Navigate(typeof(Views.PersonalInfo));
                 }
 
             }
         }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Results();
+        }
+
     }
 }
